b3ca10b2ac7d7fcad3ef1892286a7510112aadd0
Nina Lisitsa <Ninajl1@Ninas-MacBook-Pro.local>
2015-02-17 13:40:55 -0700

    User must log in before accessing Clubs and Student Views. User cannot access clubs they are not part of

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,11 +1,14 @@
 class ClubsController < ApplicationController
-
+  before_action :authenticate
   def index
     @clubs = Club.all
   end
 
   def show
     @club = Club.find(params[:id])
+    if !@club.students.find_by_id(current_student.id)
+      render 'public/403.html'
+    end
   end
 
 end
